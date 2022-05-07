364dd816dfdf0f3b32121eeb714ea1b9b805de1f
Your Name <hriosi@unal.edu.co>
2022-05-06 21:48:04 -0500

    Organizo

--- Taller2_EnClase/TG/Validacion.js
+++
@@ -1,7 +0,0 @@
-function validarEmail(correo) {
-    if (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,11}$/.test(correo)){
-     alert("La dirección de email es valida.");
-    } else {
-     alert("La dirección de email "+correo+" es invalida.");
-    }
-  }
