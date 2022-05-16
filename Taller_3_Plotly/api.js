ef8a5699e02350ca462408d270b8966a1f33873e
Your Name <hriosi@unal.edu.co>
2022-05-15 22:46:32 -0500

    Actualizo

--- Taller_3_Plotly/api.js
+++ Taller_3_Plotly/api.js
@@ -40,7 +40,6 @@ var graf1={
           {target: "OTRO", value: {marker: {color: 'red'}}},
       ]
      }]
-
 };
 
 var datosGraficas= [graf1];
@@ -55,4 +54,41 @@ var layout={
 Plotly.newPlot("myDiv",datosGraficas,layout);
 });
 
-  
+var ann = [];
+var prov = [];
+
+fetch('https://www.datos.gov.co/resource/3ebi-d83g.json')
+
+.then(obtenidos => obtenidos.json())
+
+.then(function transformar(obtenidos){
+
+x = 0
+obtenidos.forEach(function agregar (obtenidos)
+{
+    if(obtenidos.dpto != undefined && obtenidos.anio != undefined)
+    {
+        ann.push(obtenidos.anio);
+        prov.push(obtenidos.dpto);
+        console.log(prov);
+    }
+});
+
+var graf2={
+  values: ann,
+  labels: prov,
+  type: 'pie',
+};
+
+var datosgraf= [graf2];
+
+var layout={
+    title: 'Numero De Suscriptores Por Proveedor',
+    showlegend: true,
+    height: 1500,
+    width: 1500,
+    grid: {columns: 2}
+
+};
+Plotly.newPlot("grafica",datosgraf,layout);
+});
